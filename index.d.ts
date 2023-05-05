220ce23044a7dd6f4d71d3708d37d307405c408f
Liam White <liamthomaswhite1@gmail.com>
2023-05-05 08:19:15 -0700

    update d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
-declare module '@tastyworks/wallet-address-validator' {
-    const CAValidator: {
-      isCurrencySupported(coin: string): boolean
-      validate(value: string, coin: string): boolean
-    }
-    export default CAValidator
-  }
+declare const CAValidator: {
+    isCurrencySupported(coin: string): boolean,
+    validate(value: string, coin: string): boolean
+}
+
+export default CAValidator
